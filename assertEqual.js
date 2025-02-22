0783a8165c8855afafb36dded4eb23930b00ffdc
Michelle Galindo <michellegalindo2599@gmail.com>
2025-02-21 20:45:09 -0500

    assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,11 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
     return;
   }
 
-  console.log("❌❌❌ Assertion Failed: " + actual + " !== " + expected);
-
+  console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
 };
 
 //TEST CODE//
