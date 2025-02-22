35ac6835fb65c52224b9de5a70760df86cb0b26c
Michelle Galindo <michellegalindo2599@gmail.com>
2025-02-22 14:36:10 -0500

    head.js

---
+++ head.js
@@ -0,0 +1,18 @@
+const head = function(arr) {
+  return arr[0]; // Return the first element of the array
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+
+
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
