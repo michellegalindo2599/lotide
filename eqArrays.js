66f38f4a07e954a5c70ab6fa287cf4ac51d874bd
Michelle Galindo <michellegalindo2599@gmail.com>
2025-02-28 18:15:43 -0500

    eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    return;
+  }
+
+  console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const eqArrays = function(actual, expected) {
+  if(actual.length !== expected.length) {
+    console.log(false);
+    return false;
+  }
+
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      console.log(false); 
+      return false;
+    }
+  }
+    return true;
+  };
+
+
+  assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
